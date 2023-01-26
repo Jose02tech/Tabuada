610f91d837e36ad430c95844d4d74180f4ddc533
José Luiz <117102923+Jose02tech@users.noreply.github.com>
2023-01-26 00:25:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+tabu = int(input('Digite um número e saiba sua tabuada: '))
+#1 a 5
+print('{} x 1  =  \033[30m{}\033[m\n{} x 2  =  \033[31m{}\033[m\n{} x 3  =  \033[32m{}\033[m\n{} x 4  =  \033[33m{}\033[m\n{} x 5  =  \033[34m{}\033[m'.format(tabu, tabu * 1, tabu, tabu * 2, tabu, tabu * 3, tabu, tabu * 4, tabu, tabu * 5))
+#6 a 10
+print('{} x 6  =  \033[35m{}\033[m\n{} x 7  =  \033[36m{}\033[m\n{} x 8  =  \033[37m{}\033[m\n{} x 9  =  \033[38m{}\033[m\n{} x 10 =  \033[39m{}\033[m'.format(tabu, tabu * 6, tabu, tabu * 7, tabu, tabu * 8, tabu, tabu * 9, tabu, tabu * 10))
